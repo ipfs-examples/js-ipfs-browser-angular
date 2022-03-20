4dbd0c28e2b99bb492b34870af5471fb76d0dd71
tcd <thecyberd3m0n@gmail.com>
2022-03-20 22:32:14 +0100

    cleanup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -33,28 +33,3 @@ describe('AppComponent', () => {
     expect(compiled.querySelector('.content span')?.textContent).toContain('browser-angular app is running!');
   });
 });
-10
-100
-1,000
-10.000
-100,000
-1,000,000
-10,000,000
-100,000,000
-1,000,000,000
-0
-82
-119
-1,124
-10,000
-135,746
-1,000,000
-64,734,968
-187,390,524
-1,122,746,968
-100 00
-fhjhgbigjijdfinkjgibjhvuigvhfughb mktyjkkjtjitiyjkikgjthfijgdfrgryhrhtfkodljmhmtkngrehghhrjjtgYMILKYJTYJJUUhrtgjhy6ytkki956ihyhykoukoi
-hrgtfgfgghhyg utjjgUUOUU&KKJKJT^UVGH
-jgj
-jljiofjifgjiyhjiyito tyjturjiy
-,lyhjifjdyhkj,uuj7 jiortgkyiytkkiyikjoykkgkgkkjkyjkkgjfjjjgjjgbjngjjgjkghkgkgjjgjgklkgkokukythkohkkhiygiyikyiIIKiguuuyuyyyu766uyyuhtjgu754757868747757776mmb,bmm,nmn,nvjbmmbhjhmhmmhmmnmn
